5fd26c8afb65e2ad6430dfa53a1558246bd0d5bf
Max Base <MaxBaseCode@Gmail.Com>
2019-04-15 22:21:56 +0430

    Update sample.php

--- sample.php
+++ sample.php
@@ -1,1 +1,11 @@
 <?php
+/**
+*
+* @Name : PHPMultiExplode/sample.php
+* @Version : 1.0
+* @Programmer : Max
+* @Date : 2019-04-15
+* @Released under : https://github.com/BaseMax/PHPMultiExplode/blob/master/LICENSE
+* @Repository : https://github.com/BaseMax/PHPMultiExplode
+*
+**/
